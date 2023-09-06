310de82d4d7f2921180fe97c46ef409227b5f158
Alli Edwards <edwardsallistair15@gmail.com>
2023-09-06 16:57:24 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+iris = load_iris()
+
+df = pd.DataFrame(iris.data,columns=iris.feature_names)
+df['target'] = iris.target
+df['flower_name'] = df.target.apply(lambda x: iris.target_names[x])
+df0 = df[:50]
+df1 = df[50:100]
+df2 = df[100:]
+
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.scatter(df0['sepal length (cm)'], df0['sepal width (cm)'],color="green",marker='+')
+plt.scatter(df1['sepal length (cm)'], df1['sepal width (cm)'],color="blue",marker='.')
+plt.show()
+
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+plt.scatter(df0['petal length (cm)'], df0['petal width (cm)'],color="green",marker='+')
+plt.scatter(df1['petal length (cm)'], df1['petal width (cm)'],color="blue",marker='.')
+plt.show()
+
+# train test split
+x = df.drop(['target','flower_name'], axis='columns')
+y = df.target
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+# knn
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x_train, y_train)
+print(knn.score(x_test, y_test))
+
+# analysis
+from sklearn.metrics import confusion_matrix
+y_pred = knn.predict(x_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+
+
+
+
+
